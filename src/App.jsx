c11fd93f4e05434c936737f58ae7bbd0fa558289
adrian1lara <adrianlara203@gmail.com>
2023-10-04 21:02:34 -0500

    change experience and education dic for a list

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,16 @@ import EducationForm from "./components/educationalInfo"
 import GeneralInfo from "./components/generalInfo"
 import ExperienceForm from "./components/experienceInfo"
 import Resume from "./components/resume"
+import Header from "./components/header"
 
 function App() {
   const [resumeData, setResumeData] = useState({
     name: "",
     email: "",
     phone: "",
-    experience: {
-      company: "",
-      position: "",
-      duration: "",
-      description: "",
-    },
-    education: {
-      school: "",
-      degree: "",
-      date: "",
-    }
+    link: "",
+    experience: [],
+    education: [], 
   })
 
   const updateResume = (data) => {
@@ -35,14 +28,24 @@ function App() {
 
   return (
     <>
-      <Flex>
-        <Container maxW="600px" p={4} >
-        <GeneralInfo updateResume={updateResume} />
-        <EducationForm  updateResume={updateResume} />
-        <ExperienceForm  updateResume={updateResume} />
+    <Container 
+    display={"flex"} 
+    flexDirection={"column"} 
+    maxW={"container.lg"}
+    bg={"blackAlpha.50"}
+    >
+      <Header />
+      <Flex p={6}>
+        <Container maxW="600px">
+          <GeneralInfo updateResume={updateResume} />
+          <ExperienceForm  updateResume={updateResume} />
+          <EducationForm  updateResume={updateResume} />
         </Container>
-        <Resume resumeData={resumeData}/>
+        <Container >
+          <Resume resumeData={resumeData}/>
+        </Container>
       </Flex>
+    </Container>
     </>
   )
 }
